c1f19853ded4b8ed14b359f56414ee27f3faf4fe
maka <maka.saparashvili@gmail.com>
2025-10-16 16:31:59 +0400

    Views created

--- library_project/books/views.py
+++ library_project/books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+
+class BookDetailView(APIView):
+    def get(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
