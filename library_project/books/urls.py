9e8d7e1ea59d5f7390e7a3d7b9c39f85a4308694
maka <maka.saparashvili@gmail.com>
2025-10-16 16:32:55 +0400

    Urls Updated

---
+++ library_project/books/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import book_list, BookDetailView
+
+urlpatterns = [
+    path('books/', book_list, name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+]
